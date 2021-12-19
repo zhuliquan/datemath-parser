033d54defdb7186985685af160fbf346373c8136
zhuliquan <zlq164114@sina.com>
2021-12-19 23:02:07 +0800

    test: add test cases for time zone option

--- datemath_parser_test.go
+++ datemath_parser_test.go
@@ -60,5 +60,70 @@ func TestDateMathParser_evalDur(t *testing.T) {
 			}
 		})
 	}
+}
 
+func TestDateMathParser_timeZone(t *testing.T) {
+	type testCase struct {
+		name string
+		in   string
+		out  int64
+		err  error
+	}
+
+	for _, eachCase := range []testCase{
+		{
+			name: "TestTimeZone01",
+			in:   "+7:00",
+			out:  -int64(time.Hour*7) / int64(time.Second),
+			err:  nil,
+		},
+		{
+			name: "TestTimeZone02",
+			in:   "+7:45",
+			out:  -int64(time.Hour*7+time.Minute*45) / int64(time.Second),
+			err:  nil,
+		},
+		{
+			name: "TestTimeZone03",
+			in:   "-7:00",
+			out:  int64(time.Hour*7) / int64(time.Second),
+			err:  nil,
+		},
+		{
+			name: "TestTimeZone04",
+			in:   "-7:45",
+			out:  int64(time.Hour*7+time.Minute*45) / int64(time.Second),
+			err:  nil,
+		},
+		{
+			name: "TestTimeZone05",
+			in:   "PST",
+			out:  int64(time.Hour*8) / int64(time.Second),
+			err:  nil,
+		},
+		{
+			name: "TestTimeZone06",
+			in:   "Europe/Malta",
+			out:  -int64(time.Hour) / int64(time.Second),
+			err:  nil,
+		},
+		{
+			name: "TestTimeZone07",
+			in:   "Asia/Shanghai",
+			out:  -int64(time.Hour*8) / int64(time.Second),
+			err:  nil,
+		},
+	} {
+		t.Run(eachCase.name, func(t *testing.T) {
+			p, err := NewDateMathParser(WithTimeZone(eachCase.in))
+			if err != nil {
+				t.Errorf("failed to generate date math parser, err: %+v", err)
+			}
+			if o, e := p.parseTime("1970-01-01 00:00:00"); e != eachCase.err {
+				t.Errorf("expect get err: %+v, but get err: %+v", eachCase.err, e)
+			} else if o.Unix() != eachCase.out {
+				t.Errorf("expect get res: %+v, but get res: %+v", eachCase.out, o.Unix())
+			}
+		})
+	}
 }
