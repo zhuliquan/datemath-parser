3d04f9816cf2babfd6e363a436cd7536940ce453
zhuliquan <zlq164114@sina.com>
2022-05-02 23:30:57 +0800

    fix: add range checker for time zone in time offset format

--- option.go
+++ option.go
@@ -10,78 +10,48 @@ import (
 
 type DateMathParserOption func(*DateMathParser) error
 
-func changeTimeFormat(format string) (string, error) {
-	if !strings.Contains(format, "ww") ||
-		!strings.Contains(format, "xxxx") ||
-		!strings.Contains(format, "DDD") {
-		return "", fmt.Errorf("doesn't support xxxx/ww/DDD")
-	}
-	format = strings.ReplaceAll(format, "yyyy", "2006")
-	format = strings.ReplaceAll(format, "YYYY", "2006")
-	format = strings.ReplaceAll(format, "yy", "06")
-	format = strings.ReplaceAll(format, "YY", "06")
-	format = strings.ReplaceAll(format, "MM", "05")
-	format = strings.ReplaceAll(format, "dd", "04")
-	format = strings.ReplaceAll(format, "HH", "03")
-	format = strings.ReplaceAll(format, "mm", "02")
-	format = strings.ReplaceAll(format, "ss", "01")
-	format = strings.ReplaceAll(format, "e", "Mon")
-	format = strings.ReplaceAll(format, "am", "pm")
-	format = strings.ReplaceAll(format, "AM", "PM")
-	format = strings.ReplaceAll(format, ".SSSSSSSSS", ".000000000")
-	format = strings.ReplaceAll(format, ".SSSSSSSS", ".00000000")
-	format = strings.ReplaceAll(format, ".SSSSSSS", ".0000000")
-	format = strings.ReplaceAll(format, ".SSSSSS", ".000000")
-	format = strings.ReplaceAll(format, ".SSSSS", ".00000")
-	format = strings.ReplaceAll(format, ".SSSS", ".0000")
-	format = strings.ReplaceAll(format, ".SSS", ".000")
-	format = strings.ReplaceAll(format, ".SS", ".00")
-	format = strings.ReplaceAll(format, ".S", ".0")
-	return format, nil
-}
-
 func WithFormat(formats []string) DateMathParserOption {
 	return func(p *DateMathParser) error {
-		var resF = []string{}
+		var parserFormats = []string{}
 		for _, format := range formats {
-			if builtInFormat[format] != nil {
-				for _, bf := range builtInFormat[format] {
-					if bf, err := changeTimeFormat(bf); err == nil {
-						resF = append(resF, bf)
-					}
-				}
+			if jodaFormats, ok := builtInFormat[format]; ok {
+				parserFormats = append(parserFormats, jodaFormats...)
 			} else {
-				if format, err := changeTimeFormat(format); err == nil {
-					resF = append(resF, format)
-				}
+				parserFormats = append(parserFormats, format)
 			}
 		}
-		p.Formats = resF
+		p.Formats = parserFormats
 		return nil
 	}
 }
 
-var TimeZoneOffset = regexp.MustCompile(`(\+|-)(\d+):(\d+)`)
+var TimeZoneOffset = regexp.MustCompile(`(\+|-)(\d{1,2}):(\d{1,2})`)
 
 func WithTimeZone(timeZone string) DateMathParserOption {
 	return func(p *DateMathParser) error {
 		if loc, err := time.LoadLocation(timeZone); err != nil {
-			timeZone = strings.ToUpper(timeZone)
-			if builtInTimeZone[timeZone] != "" {
-				timeZone = builtInTimeZone[timeZone]
+			var timeOffset = timeZone
+			if t, ok := builtInTimeZone[strings.ToUpper(timeZone)]; ok {
+				timeOffset = t
 			}
-			var s = TimeZoneOffset.FindStringSubmatch(timeZone)
+			var s = TimeZoneOffset.FindStringSubmatch(timeOffset)
 			if len(s) != 4 {
-				return fmt.Errorf("time zone: %s is invalid", timeZone)
+				return fmt.Errorf("time zone: %s format is invalid, expect time offset format: (\\+|-)(\\d{1,2}):(\\d{1,2}) or time zone (abbreviation/full name) or IANA format", timeZone)
 			} else {
-				var offset = 1
+				var flag = 1
 				if s[1] == "-" {
-					offset = -1
+					flag = -1
 				}
 				var hour, _ = strconv.Atoi(s[2])
+				if hour > 23 || hour < 0 {
+					return fmt.Errorf("time zone: %s is invalid, hour is out of range [0, 23]", timeZone)
+				}
 				var minute, _ = strconv.Atoi(s[3])
-				offset = offset * (hour*int(time.Hour) + minute*int(time.Minute)) / int(time.Second)
-				p.TimeZone = time.FixedZone("UTC", offset)
+				if minute > 59 || minute < 0 {
+					return fmt.Errorf("time zone: %s is invalid, minute is out of range [0, 59]", timeZone)
+				}
+				p.TimeZone = time.FixedZone("UTC",
+					flag*(hour*int(time.Hour)+minute*int(time.Minute))/int(time.Second))
 				return nil
 			}
 		} else {
